02faa9a57f8fecca0f6a49912c204032bf59b4dc
Semiha Beyazkilic <semiha.beyazkilic@gmail.com>
2023-09-23 16:05:17 +0200

    AContext

--- server/src/conf/AContext.hpp
+++ server/src/conf/AContext.hpp
@@ -19,41 +19,32 @@ class AContext {
 		size_t										getMaxBodySize() const;
 		std::string const&							getRoot() const;
 
-		std::map<std::string, int> const&			getListen(void) const;
 		std::map<int, std::string> const&			getErrorPages() const;
-		std::map<std::string, std::string> const&	getLocations() const;
 
 		std::vector<std::string> const&				getIndex() const;
 		std::vector<std::string> const&				getAuthorizedMethods() const;
-		std::vector<std::string> const&				getServerNames(void) const;
 
 		void	setAutoindex(bool autoindex);
 		void	setCgi(bool cgi);
 		void	setMaxBodySize(size_t size);
 		void	setRoot(std::string const& root);
 
-		void	setListen(std::string const& ip, int port);
-		void	setErrorPage(std::map<int, std::string> const& errorPages);
 		void	addErrorPage(int code, std::string const& path);
-		void	setLocation(std::string const& location, std::string const& root);
+		void	setErrorPage(std::map<int, std::string> const& errorPages);
 
 		void	setIndex(std::vector<std::string> const& index);
 		void	setAuthorizedMethods(std::vector<std::string> const& authorizedMethods);
-		void	setServerNames(std::vector<std::string> const& serverNames);
 
 	private:
 		bool								_autoindex;
 		bool								_cgi;
 		size_t								_maxBodySize;
-		std::string							_root; //
+		std::string							_root;
 
-		std::map<std::string, int> 			_listen; // <IP, port>
 		std::map<int, std::string>			_errorPages;
-		std::map<std::string, std::string>	_locations; // special : location context. Class?
 		
 		std::vector<std::string>			_index;
 		std::vector<std::string>			_authorizedMethods;
-		std::vector<std::string>			_serverNames;
 
 };
 
--- server/src/conf/ServerContext.hpp
+++ server/src/conf/ServerContext.hpp
@@ -11,45 +11,17 @@ class ServerContext : public AContext {
 		ServerContext& operator=(ServerContext const& rhs);
 		~ServerContext();
 
-		bool										getAutoindex() const;
-		bool										getCgi() const;
-		size_t										getMaxBodySize() const;
-		std::string const&							getRoot() const;
-
 		std::map<std::string, int> const&			getListen(void) const;
-		std::map<int, std::string> const&			getErrorPages() const;
 		std::map<std::string, std::string> const&	getLocations() const;
-
-		std::vector<std::string> const&				getIndex() const;
-		std::vector<std::string> const&				getAuthorizedMethods() const;
 		std::vector<std::string> const&				getServerNames(void) const;
 
-		void	setAutoindex(bool autoindex);
-		void	setCgi(bool cgi);
-		void	setMaxBodySize(size_t size);
-		void	setRoot(std::string const& root);
-
 		void	setListen(std::string const& ip, int port);
-		void	setErrorPage(std::map<int, std::string> const& errorPages);
-		void	addErrorPage(int code, std::string const& path);
 		void	setLocation(std::string const& location, std::string const& root);
-
-		void	setIndex(std::vector<std::string> const& index);
-		void	setAuthorizedMethods(std::vector<std::string> const& authorizedMethods);
 		void	setServerNames(std::vector<std::string> const& serverNames);
 
 	private:
-		bool								_autoindex;
-		bool								_cgi;
-		size_t								_maxBodySize;
-		std::string							_root;
-
 		std::map<std::string, int> 			_listen; // <IP, port>
-		std::map<int, std::string>			_errorPages;
-		std::map<std::string, std::string>	_locations; // TODO: change to map<string, LocationContext>
-		
-		std::vector<std::string>			_index;
-		std::vector<std::string>			_authorizedMethods;
+		std::map<std::string, std::string>	_locations; // TODO: change to map<string, LocationContext>		
 		std::vector<std::string>			_serverNames;
 
 };
